03ba6a4feca6054e13d27406c629ce9d51e65861
WanDanLou <liliac119@gmail.com>
2018-06-06 23:57:24 +0800

    update .gitignore

--- 19.sql
+++ 19.sql
@@ -1,7 +1,6 @@
--- $ID$
--- TPC-H/TPC-R Discounted Revenue Query (Q19)
--- Functional Query Definition
--- Approved February 1998
+﻿-- count 122
+-- Lineitem 主键 L_ORDERKEY,L_LINENUMBER /6001215
+-- part 主键 P_PARTKEY /200000
 :x
 :o
 select
@@ -10,32 +9,33 @@ from
   lineitem,
   part
 where
-  (
-    p_partkey = l_partkey
-    and p_brand = 'Brand#33'
-    and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
-    and l_quantity >= 8 and l_quantity <= 8 + 10
-    and p_size between 1 and 5
-    and l_shipmode in ('AIR', 'AIR REG')
-    and l_shipinstruct = 'DELIVER IN PERSON'
-  )
-  or
-  (
-    p_partkey = l_partkey
-    and p_brand = 'Brand#32'
-    and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
-    and l_quantity >= 19 and l_quantity <= 19 + 10
-    and p_size between 1 and 10
-    and l_shipmode in ('AIR', 'AIR REG')
-    and l_shipinstruct = 'DELIVER IN PERSON'
-  )
-  or
-  (
-    p_partkey = l_partkey
-    and p_brand = 'Brand#13'and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
-    and l_quantity >= 26 and l_quantity <= 26 + 10
-    and p_size between 1 and 15
-    and l_shipmode in ('AIR', 'AIR REG')
-    and l_shipinstruct = 'DELIVER IN PERSON'
-  );
+(
+p_partkey = l_partkey
+and p_brand = 'Brand#33'
+and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
+and l_quantity >= 8 and l_quantity <= 8 + 10
+and p_size between 1 and 5
+and l_shipmode in ('AIR', 'AIR REG')
+and l_shipinstruct = 'DELIVER IN PERSON'
+)
+or
+(
+p_partkey = l_partkey
+and p_brand = 'Brand#32'
+and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
+and l_quantity >= 19 and l_quantity <= 19 + 10
+and p_size between 1 and 10
+and l_shipmode in ('AIR', 'AIR REG')
+and l_shipinstruct = 'DELIVER IN PERSON'
+)
+or
+(
+p_partkey = l_partkey
+and p_brand = 'Brand#13'
+and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
+and l_quantity >= 26 and l_quantity <= 26 + 10
+and p_size between 1 and 15
+and l_shipmode in ('AIR', 'AIR REG')
+and l_shipinstruct = 'DELIVER IN PERSON'
+);
 :n -1
